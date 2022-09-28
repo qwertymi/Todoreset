a37bf90ba5c3dc2b323418a14f320b5c435cc006
hemhem <kimhp1216@gmail.com>
2022-09-28 13:07:16 +0900

    수정기능

---
+++ src/components/ToastBox.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="alert toast-box" :class="`alert-${color}`" role="alert">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: "success",
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style>
+.toast-box {
+  position: fixed;
+  top: 8px;
+  right: 10px;
+  height: 40px;
+  padding-top: 7px;
+}
+</style>
